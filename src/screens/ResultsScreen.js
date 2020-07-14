00fd30bac93bcacf9a02fa25abf1b2234703f971
mdb1710 <mdb1710@gmail.com>
2020-07-14 14:05:22 -0400

    rendered terms on results pages

--- src/screens/ResultsScreen.js
+++ src/screens/ResultsScreen.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native'
+import CalcContext from '../context/CalcContext'
 
 const ResultsScreen = () => {
+  const { searchTerms } = useContext(CalcContext)
+
+  console.log('Final terms are', searchTerms);
+
+  const { selectedStreams, selectedService, selectedMonths } = searchTerms
+
   return(
     <View>
       <Text>We will show the results here</Text>
+      <Text>Streams: {selectedStreams}</Text>
+      <Text>Service: {selectedService}</Text>
+      <Text>Months: {selectedMonths}</Text>
     </View>
   )
 };
