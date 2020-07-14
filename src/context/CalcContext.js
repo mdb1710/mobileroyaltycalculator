bafd1b1102529db58978bdd5acfd340945042839
mdb1710 <mdb1710@gmail.com>
2020-07-14 11:53:50 -0400

    config CalcContext and Formscreen for useReducer

--- src/context/CalcContext.js
+++ src/context/CalcContext.js
@@ -2,20 +2,28 @@ import React, { useReducer } from 'react'
 
 const CalcContext = React.createContext();
 
-const searchTerms = [
-  {selectedStreams: ''},
-  {selectedService: ''},
-  {selectedMonths: ''}
-];
+// const searchTerms = [
+//   {selectedStreams: ''},
+//   {selectedService: ''},
+//   {selectedMonths: ''}
+// ];
+
+const searchTerms = {
+  selectedStreams: '',
+  selectedService: '',
+  selectedMonths: ''
+}
 
 const searchReducer = (state, action) => {
   switch (action.type) {
     case 'add_streams':
-      return [ ...state, selectedStreams: action.payload];
+      return { ...state, selectedStreams: action.payload };
     case 'add_service':
-      return [ ...state, selectedService: action.payload];
+      return { ...state, selectedService: action.payload };
+      // return [ ...state, {selectedService: action.payload}];
     case 'add_months':
-      return [...state, selectedMonths: action.payload];
+      return { ...state, selectedMonths: action.payload };
+      // return [...state, {selectedMonths: action.payload}];
     default:
       return state;
   }
@@ -25,21 +33,25 @@ const searchReducer = (state, action) => {
 export const CalcProvider = ({ children }) => {
   const [state, dispatch] = useReducer(searchReducer, searchTerms);
 
-  const addStreams = streams = {
-    dispatch({ type: 'add_streams', payload: streams });
+  // const addStreams = (streams) = {
+  //   dispatch({ type: 'add_streams', payload: streams });
+  // }
+
+  const addStreams = streams => {
+    dispatch({ type: 'add_streams', payload: streams })
   }
 
-  const addService = service = {
-    dispatch({ type: 'add_service', payload: service });
+  const addService = service => {
+    dispatch({ type: 'add_service', payload: service })
   }
 
-  const addMonths = months = {
+  const addMonths = months => {
     dispatch({ type: 'add_months', payload: months });
   }
 
   //state = searchTerms
 
-  return <CalcContext.Provider value={searchTerms, addStreams, addService, addMonths}>{children}</CalcContext.Provider>
+  return <CalcContext.Provider value={{ searchTerms, addStreams, addService, addMonths }}>{children}</CalcContext.Provider>
 };
 
 export default CalcContext;
--- src/screens/FormScreen.js
+++ src/screens/FormScreen.js
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
+import CalcContext from '../context/CalcContext'
 
 const FormScreen = ({ navigation }) => {
   const [streams, setStreams] = useState('');
   const { label, input } = styles;
 
+  const { searchTerms, addStreams } = useContext(CalcContext)
   // const onPress = () => {
   //   console.log('this many', streams);
   //
   // }
 
+  const directToService = () => {
+    addStreams(streams)
+    console.log(searchTerms)
+    navigation.navigate('Service')
+  }
+
+  const handleStreamChange = number => {
+    console.log(number)
+    setStreams(number);
+
+  }
+
   return(
     <View>
       <Text>Enter The Number of Streams</Text>
-      <TextInput style={input} value={streams} onChangeText={(text) => setStreams(text)}/>
+      <TextInput style={input} value={streams} onChangeText={(text) => handleStreamChange(text)}/>
       <Text>You chose {streams}</Text>
-      <Button title="Go To Service" onPress={() => navigation.navigate('Service')}/>
+      <Button title="Go To Service" onPress={() => directToService()}/>
     </View>
   )
 };
