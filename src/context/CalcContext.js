f057c42beee4dbd6f4cf631c51a3f8cc7917c63c
mdb1710 <mdb1710@gmail.com>
2020-07-14 01:11:33 -0400

    added useReducer to Calccontext

--- src/context/CalcContext.js
+++ src/context/CalcContext.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 
 const CalcContext = React.createContext();
 
+const searchTerms = [
+  {selectedStreams: ''},
+  {selectedService: ''},
+  {selectedMonths: ''}
+];
+
+const searchReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_streams':
+      return [ ...state, selectedStreams: action.payload];
+    case 'add_service':
+      return [ ...state, selectedService: action.payload];
+    case 'add_months':
+      return [...state, selectedMonths: action.payload];
+    default:
+      return state;
+  }
+
+};
+
 export const CalcProvider = ({ children }) => {
-  return <CalcContext.Provider>{children}</CalcContext.Provider>
+  const [state, dispatch] = useReducer(searchReducer, searchTerms);
+
+  const addStreams = streams = {
+    dispatch({ type: 'add_streams', payload: streams });
+  }
+
+  const addService = service = {
+    dispatch({ type: 'add_service', payload: service });
+  }
+
+  const addMonths = months = {
+    dispatch({ type: 'add_months', payload: months });
+  }
+
+  //state = searchTerms
+
+  return <CalcContext.Provider value={searchTerms, addStreams, addService, addMonths}>{children}</CalcContext.Provider>
 };
 
 export default CalcContext;
