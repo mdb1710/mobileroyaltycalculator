8617fb1bd75088d127a2f63c0c30ac96a502ee41
mdb1710 <mdb1710@gmail.com>
2020-07-14 23:48:55 -0400

    adding ui to welcomescreen

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import { SafeAreaView, Text, StyleSheet, Button } from 'react-native'
+import { SafeAreaView, StyleSheet } from 'react-native'
+
+import { Button, Text } from 'react-native-elements';
 
 const WelcomeScreen = ({ navigation }) => {
   return(
     <SafeAreaView style={styles.welcomeText}>
-      <Text>Welcome to your Calculator</Text>
-      <Button title="Start Here" onPress={() => navigation.navigate('formFlow')}/>
+      <Text h3>Streaming Royalty Calculator</Text>
+      <Text>Indie artists don't have to guess how much money one can make off of monthly streams. With this calculator you can find out right how.</Text>
+      <Text>Just click the button below to start</Text>
+      <Button title="Start Here" buttonStyle={styles.button} onPress={() => navigation.navigate('formFlow')}/>
     </SafeAreaView>
   )
 };
 
 const styles = StyleSheet.create({
   welcomeText: {
-    fontSize: 48
+    fontSize: 48,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    width: '80%',
+    marginTop: 10,
+    marginBottom: 10
   }
 });
 
