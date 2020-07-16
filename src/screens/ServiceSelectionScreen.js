70b2bb34b4bd7e2daf8dea2f4f6312f0b69c4595
mdb1710 <mdb1710@gmail.com>
2020-07-16 14:43:38 -0400

    added rnelements to serviceselectionscreen

--- src/screens/ServiceSelectionScreen.js
+++ src/screens/ServiceSelectionScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import { Text, Button } from 'react-native-elements';
 import CalcContext from '../context/CalcContext'
 
 const ServiceSelectionScreen = ({ navigation }) => {
@@ -22,17 +23,23 @@ const ServiceSelectionScreen = ({ navigation }) => {
   }
   return (
     <SafeAreaView>
-      <Text>Choose Your Service</Text>
-      <Button title="Apple Music" onPress={() => handleServiceChange('Apple Music')}/>
-      <Button title="YouTube" onPress={() => handleServiceChange('YouTube')}/>
-      <Button title="Soundcloud" onPress={() => handleServiceChange('Soundcloud')}/>
-      <Button title="Spotify" onPress={() => handleServiceChange('Spotify')}/>
-      <Text>You Chose {service}</Text>
-      <Button title="Go To Months" onPress={() => directToMonths()}/>
+      <Text h3>Choose Your Service</Text>
+      <Button title="Apple Music" buttonStyle={styles.button} onPress={() => handleServiceChange('Apple Music')}/>
+      <Button title="YouTube" buttonStyle={styles.button} onPress={() => handleServiceChange('YouTube')}/>
+      <Button title="Soundcloud" buttonStyle={styles.button} onPress={() => handleServiceChange('Soundcloud')}/>
+      <Button title="Spotify" buttonStyle={styles.button} onPress={() => handleServiceChange('Spotify')}/>
+      <Text h4>You Chose {service}</Text>
+      <Button title="Go To Months" buttonStyle={styles.button} onPress={() => directToMonths()}/>
     </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  button: {
+    width: '80%',
+    marginTop: 10,
+    marginBottom: 10
+  }
+});
 
 export default ServiceSelectionScreen;
